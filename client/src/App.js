2d8b0b58b3836406444ebc88b5b4f2756ec07378
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-07-12 01:57:17 +0530

    Frontend Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+
+import memories from "./images/memories.jpeg";
+
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
 
 const App = () => {
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post.js";
+
+const Posts = () => {
+  return (
+    <>
+      <h1>Post</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
